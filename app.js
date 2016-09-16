6630ee7df33d229af5a769f0434ab28466cc2fec
Jeremy Katras <jakatras@gmail.com>
2016-09-16 11:58:55 -0400

    improve operator logic; solution returned behavior changed, (cont)
    -default operation chain is 0
    -added comments to JS

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+//main display
 var display = document.getElementsByClassName('display')[0];
-var opChain = [];
+//sub display
+var displaySub = document.getElementsByClassName('display-sub')[0];
+//chain of operations, default is zero
+var opChain = [0];
+//keeps track of whether solution is returned
+var solutionReturned = false;
+//all buttons
 var btns = document.getElementsByTagName('button');
 
-//add click events to each button to capture values
+//add click events to each button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', getBtnValue);
 }
-
+//get button value and send it to be processed
 function getBtnValue(e) {
   var entry = e.currentTarget.value;
   processInput(entry);
 }
-
-function processInput(input){
-  switch(input){
+//process input based on button pressed, perform next appropriate operation
+function processInput(input) {
+  switch (input) {
     case "=":
       calculate();
       break;
@@ -34,48 +41,99 @@ function processInput(input){
     default:
       updateDisplay(input);
   }
-} 
-function updateDisplay(input){
-  if(display.innerHTML === "0" && input !== "0" || opChain.length == 0){
-    display.innerHTML = input;
-  }else{
+}
+//function determines how display will be updated
+function updateDisplay(input) {
+  //if display is blank and input isn't zero or solution has been returned
+  if ((display.innerHTML === "0" && input !== "0") || opChain === 0 || solutionReturned === true) {
+    //if solution is displayed and input is an operator or display is blank and input is an operator
+    if ((solutionReturned === true && !isNumeric(input)) || (display.innerHTML === "0" && !isNumeric(input))) {
+      //set solution to false because operation chain is increasing
+      solutionReturned = false;
+      //add input to the chain
+      chainOperation(input);
+      //add input to the display
+      display.innerHTML += input;
+    } else {
+      //if above conditions not met, clear the chain, input replaces blank display state of 0 and replaces default 0 in operation chain
+      solutionReturned = false;
+      clearOpChain();
+      display.innerHTML = input;
+      opChain.push(input);
+    }
+  } else {
+    //if not starting from blank state, add input to display and operation chain
     display.innerHTML += input;
+    chainOperation(input);
   }
-  chainOperation(input);
+  displaySub.innerHTML = opChain.join("");
 }
-function validateInput(input){
-  var current = display.innerHTML;
-  if(input === "0" && current !== "0"){
+//function validates 0, operators, and decimals
+function validateInput(input) {
+  var currentDisplay = display.innerHTML;
+  //if input is zero and display isn't, or input is the first decimal in display and solution is not displayed
+  if ((input === "0" && currentDisplay !== "0") || (input === "." && currentDisplay.indexOf(".") < 0 && solutionReturned === false)) {
+    //update display
     updateDisplay(input);
   }
-  if(input === "." && current.indexOf(".") < 0){
-    updateDisplay(input);
-  }
-  if((input === "/" || input === "*" || input === "-" || input === "+") && current.charAt(current.length-1) !== input){
-    updateDisplay(input);
+  //if input is an operator
+  if (input === "/" || input === "*" || input === "-" || input === "+") {
+    var lastChar = opChain[opChain.length - 1];
+    //if last character was a number
+    if (isNumeric(lastChar)) {
+      //update display
+      updateDisplay(input);
+    }
   }
 }
-function clearEntry(){
-  if(opChain.length >= 2){
-    display.innerHTML = display.innerHTML.slice(0,-1);
-  }else{
+//checks whether given character is a number
+function isNumeric(value) {
+  return (value == Number(value));
+}
+//clears last entry
+function clearEntry() {
+  //if chain of operations is not empty
+  if (opChain.length >= 2) {
+    //remove last character
+    display.innerHTML = display.innerHTML.slice(0, -1);
+  } else {
+    //or set display to default 0
     display.innerHTML = "0";
   }
+  //remove last character from operation chain
   opChain.pop();
+  //update sub display
+  displaySub.innerHTML = opChain.join("");
 }
-function clearOpChain(){
+//clears chain of operations
+function clearOpChain() {
   opChain.length = 0;
 }
-function clearAll(){
-  opChain.length = 0;
+//clears display, sub display, and sets chain of operations to default 0
+function clearAll() {
+  opChain = [0];
   display.innerHTML = "0";
+  displaySub.innerHTML = "";
 }
-function chainOperation(input){
+//adds to chain of operations
+function chainOperation(input) {
   opChain.push(input);
 }
-function calculate(){
-  if(opChain.length !== 0){
-    display.innerHTML = eval(opChain.join(""));
-    clearOpChain();
+//returns solution
+function calculate() {
+  //if operation chain isn't empty
+  if (opChain !== 0) {
+    //evaluate operation chain
+    var solution = eval(opChain.join(""));
+    //update display with solution
+    display.innerHTML = solution;
+    //update display to reflect previous operations
+    displaySub.innerHTML += "=";
+    //reset operation chain
+    opChain.length = 0;
+    //operation chain now contains solution
+    opChain.push(solution);
+    //set global variable to indicate solution was reached
+    solutionReturned = true;
   }
 }
